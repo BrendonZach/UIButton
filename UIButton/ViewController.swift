62439e73bd292ebb3880c529175ba1640ce552bf
Brendon Zach <brendon_zach@outlook.com>
2022-01-18 13:44:29 -0700

    finished product

--- UIButton/ViewController.swift
+++ UIButton/ViewController.swift
@@ -7,12 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var myLabel: UILabel!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+
+    @IBAction func whenPressed(_ sender: Any)
+    {
+        self.view.backgroundColor = UIColor.green
+        myLabel.text = "It's not easy being green"
+        
+    }
+    
+    @IBAction func resetButton(_ sender: Any)
+    {
+        self.view.backgroundColor = UIColor.white
+        myLabel.text = "What Color Am I?"
+    }
+    
+    
 
 
 }
